0a7abb9c0a394dfa700a7c40194823d737cdee0e
LTH <johtta888@naver.com>
2023-01-11 21:20:13 +0900

    Feat: 추가 favicon 및 GA

--- src/components/@module/index.ts
+++ src/components/@module/index.ts
@@ -1,7 +1,9 @@
 export { default as ActionsHeader } from "./ActionsHeader";
+export { default as ClientProvider } from "./ClientProvider";
+export { default as GAScript } from "./GAScript";
 export { default as GlobalDialog } from "./GlobalDialog";
 export { default as Header } from "./Header";
 export { default as Layout } from "./Layout";
 export { default as Menu } from "./Menu";
 export { default as Nav } from "./Nav";
-export { default as ClientProvider } from "./ClientProvider";
+export { default as Favicon } from "./Favicon";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,45 @@ import {
 } from "@tanstack/react-query";
 import "common/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
-import { ClientProvider } from "components/@module";
+import { useEffect, useState } from "react";
+import { ClientProvider, GAScript } from "components/@module";
+import { useRouter } from "next/router";
+import * as gtag from "utils/gtag";
+import Head from "next/head";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Hydrate state={pageProps.dehydratedState}>
-        <ClientProvider>
-          <Component {...pageProps} />
-        </ClientProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <GAScript GA_ID={GA_ID} />
+      <Head>
+        <title>이름낼름</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <ClientProvider>
+            <Component {...pageProps} />
+          </ClientProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
